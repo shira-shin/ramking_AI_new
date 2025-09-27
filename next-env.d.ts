70f4f3d124e3cd00d73f0875769296d328f29776
shira-shin <shiratorien9837@gmail.com>
2025-09-27 12:59:11 +0900

    fix(types): move @types packages to dependencies

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,4 @@
-// /// <reference types="next" />
-// /// <reference types="next/image-types/global" />
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
 // NOTE: This file should not be edited
